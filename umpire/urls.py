662a89776ac629304b7d70f251bfcc610c775345
pantunelfreddy <pantun.elfreddy@ui.ac.id>
2023-05-07 18:26:58 +0700

    menambah card umpire

--- umpire/urls.py
+++ umpire/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from umpire.views import dash_umpire
+from umpire.views import show_pertandingan
 
 app_name = 'umpire'
 
 urlpatterns = [
     path('', dash_umpire, name='dash_umpire'),
+     path('pertandingan/', show_pertandingan, name='pertandingan'),
     
 ]
--- umpire/views.py
+++ umpire/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def dash_umpire(request):
     return render(request, 'umpire.html')
+
+def show_pertandingan(request):
+    return render(request, 'pertandingan.html')
