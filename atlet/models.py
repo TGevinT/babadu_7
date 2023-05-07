30b17fd6ae66f63e74a14116a5903c4dc1c83846
ratuanvilen <92584623+ratuanvilen@users.noreply.github.com>
2023-05-07 18:30:33 +0700

    atlet

--- atlet/models.py
+++ atlet/models.py
@@ -1,3 +1,13 @@
+
 from django.db import models
+
+
+class Atlet(models.Model):
+    nama = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nama
+
+
 
 # Create your models here.
--- atlet/urls.py
+++ atlet/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from atlet.views import dash_atlet
+from atlet.views import form_kualifikasi
+from atlet.views import pertanyaan_kualifikasi
+from atlet.views import daftar_atlet
+from atlet.views import list_atlet
+from atlet.views import register_sponsorship
 
 app_name = 'atlet'
 
 urlpatterns = [
     path('', dash_atlet, name='dash_atlet'),
-    
+    path('form_kualifikasi/', form_kualifikasi, name='form_kualifikasi'),
+    path('form_kualifikasi/pertanyaan_kualifikasi/', pertanyaan_kualifikasi, name='pertanyaan_kualifikasi'),
+    path('daftar_atlet/', daftar_atlet, name='daftar_atlet'),    
+    path('daftar_atlet/list_atlet/', list_atlet, name='list_atlet'),
 ]
